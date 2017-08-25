7744394256a04881643fb754c6685f6b7c368f1c
Nottrex <Lau.Nottrex@gmail.com>
2017-08-25 17:40:46 +0200

    less BufferedImage recreation

--- src/game/window/view/GameView.java
+++ src/game/window/view/GameView.java
@@ -16,13 +16,15 @@ import java.awt.image.BufferedImage;
 
 public class GameView extends View implements Controller {
 	public static final int SIZE = 64;
-
+	private BufferedImage buffer;
 	private Window w;
 	private boolean running;
-
 	private KeyInputListener key;
-
 	private Game game;
+	private BufferedImage mapBuffer;
+	private int bw = 0, bh = 0;
+	private BufferedImage gameBuffer;
+	private int gbw, gbh;
 
 	@Override
 	public void init(Window window) {
@@ -39,14 +41,14 @@ public class GameView extends View implements Controller {
 		redrawMap();
 
 		new Thread(() -> {
-			while(running) {
+			while (running) {
 				draw();
 			}
 		}).start();
 
 		new Thread(() -> {
 			long t1 = System.currentTimeMillis();
-			while(running) {
+			while (running) {
 				long t2 = System.currentTimeMillis();
 				updatePlayerWalking();
 				game.update(t2 - t1);
@@ -55,7 +57,6 @@ public class GameView extends View implements Controller {
 		}).start();
 	}
 
-	private BufferedImage mapBuffer;
 	private void redrawMap() {
 		GameMap map = game.getMap();
 
@@ -65,7 +66,7 @@ public class GameView extends View implements Controller {
 		g.setColor(Color.RED);
 		for (int x = 0; x < map.getWidth(); x++) {
 			for (int y = 0; y < map.getHeight(); y++) {
-				g.drawImage(TextureHandler.getImagePng("ground"), x*SIZE, y*SIZE, SIZE, SIZE, null);
+				g.drawImage(TextureHandler.getImagePng("ground"), x * SIZE, y * SIZE, SIZE, SIZE, null);
 
 				String wall = "";
 				boolean t = map.hasWall(x, y, Direction.UP);
@@ -85,20 +86,25 @@ public class GameView extends View implements Controller {
 				else if (r) wall = "walls_edge_r";
 
 				if (!wall.equals("")) {
-					g.drawImage(TextureHandler.getImagePng(wall), x*SIZE, y*SIZE, SIZE, SIZE, null);
+					g.drawImage(TextureHandler.getImagePng(wall), x * SIZE, y * SIZE, SIZE, SIZE, null);
 				}
 
 
-				if (t) g.drawLine(x*SIZE, y*SIZE, (x+1)*SIZE, y*SIZE);
-				if (b) g.drawLine(x*SIZE, (y+1)*SIZE, (x+1)*SIZE, (y+1)*SIZE);
-				if (l) g.drawLine(x*SIZE, y*SIZE, x*SIZE, (y+1)*SIZE);
-				if (r) g.drawLine((x+1)*SIZE, y*SIZE, (x+1)*SIZE, (y+1)*SIZE);
+				if (t) g.drawLine(x * SIZE, y * SIZE, (x + 1) * SIZE, y * SIZE);
+				if (b) g.drawLine(x * SIZE, (y + 1) * SIZE, (x + 1) * SIZE, (y + 1) * SIZE);
+				if (l) g.drawLine(x * SIZE, y * SIZE, x * SIZE, (y + 1) * SIZE);
+				if (r) g.drawLine((x + 1) * SIZE, y * SIZE, (x + 1) * SIZE, (y + 1) * SIZE);
 			}
 		}
 	}
 
 	public void draw() {
-		BufferedImage buffer = new BufferedImage(w.getPanel().getWidth(), w.getPanel().getHeight(), BufferedImage.TYPE_INT_ARGB);
+		if (w.getPanel().getWidth() != bw || w.getPanel().getHeight() != bh) {
+			buffer = new BufferedImage(w.getPanel().getWidth(), w.getPanel().getHeight(), BufferedImage.TYPE_INT_ARGB);
+			bw = w.getPanel().getWidth();
+			bh = w.getPanel().getHeight();
+		}
+
 		Graphics2D g = (Graphics2D) buffer.getGraphics();
 
 		g.setColor(Color.GRAY);
@@ -110,20 +116,24 @@ public class GameView extends View implements Controller {
 
 		GameMap map = game.getMap();
 
-		BufferedImage gameBuffer = new BufferedImage(SIZE * map.getWidth(), SIZE * map.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
+		if (SIZE * map.getWidth() != gbw || SIZE * map.getHeight() != gbh) {
+			gameBuffer = new BufferedImage(SIZE * map.getWidth(), SIZE * map.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
+			gbw = SIZE * map.getWidth();
+			gbh = SIZE * map.getHeight();
+		}
 		Graphics2D gameGraphics = (Graphics2D) gameBuffer.getGraphics();
 		gameGraphics.drawImage(mapBuffer, 0, 0, null);
 
-		gameGraphics.drawImage(TextureHandler.getImagePng("tanks_tankRed"), (int) (SIZE*game.getFirstPlayer().getX()), (int) (SIZE*game.getFirstPlayer().getY()), (int) (SIZE*game.getFirstPlayer().getWidth()),(int) (SIZE*game.getFirstPlayer().getHeight()), null);
-		gameGraphics.drawImage(TextureHandler.getImagePng("tanks_tankGreen"), (int) (SIZE*game.getSecondPlayer().getX()), (int) (SIZE*game.getSecondPlayer().getY()), (int) (SIZE*game.getSecondPlayer().getWidth()),(int) (SIZE*game.getSecondPlayer().getHeight()), null);
+		gameGraphics.drawImage(TextureHandler.getImagePng("tanks_tankRed"), (int) (SIZE * game.getFirstPlayer().getX()), (int) (SIZE * game.getFirstPlayer().getY()), (int) (SIZE * game.getFirstPlayer().getWidth()), (int) (SIZE * game.getFirstPlayer().getHeight()), null);
+		gameGraphics.drawImage(TextureHandler.getImagePng("tanks_tankGreen"), (int) (SIZE * game.getSecondPlayer().getX()), (int) (SIZE * game.getSecondPlayer().getY()), (int) (SIZE * game.getSecondPlayer().getWidth()), (int) (SIZE * game.getSecondPlayer().getHeight()), null);
 
 
-		if (gameBuffer.getWidth() / (1f*gameBuffer.getHeight()) >= buffer.getWidth() / (1f*buffer.getHeight())) {
-			float height = (gameBuffer.getHeight() / (1f*gameBuffer.getWidth())) * buffer.getWidth();
-			g.drawImage(gameBuffer, 0, (int) ((buffer.getHeight() - height)/2), buffer.getWidth(), (int) height, null);
+		if (gameBuffer.getWidth() / (1f * gameBuffer.getHeight()) >= buffer.getWidth() / (1f * buffer.getHeight())) {
+			float height = (gameBuffer.getHeight() / (1f * gameBuffer.getWidth())) * buffer.getWidth();
+			g.drawImage(gameBuffer, 0, (int) ((buffer.getHeight() - height) / 2), buffer.getWidth(), (int) height, null);
 		} else {
-			float width = (gameBuffer.getWidth() / (1f*gameBuffer.getHeight())) * buffer.getHeight();
-			g.drawImage(gameBuffer, (int) ((buffer.getWidth() - width)/2), 0, (int) width, buffer.getHeight(), null);
+			float width = (gameBuffer.getWidth() / (1f * gameBuffer.getHeight())) * buffer.getHeight();
+			g.drawImage(gameBuffer, (int) ((buffer.getWidth() - width) / 2), 0, (int) width, buffer.getHeight(), null);
 		}
 
 		g.translate(-c.x, -c.y);
@@ -136,8 +146,8 @@ public class GameView extends View implements Controller {
 		if (key.isPressed(KeyEvent.VK_A)) mx -= 1;
 		if (key.isPressed(KeyEvent.VK_W)) my -= 1;
 		if (key.isPressed(KeyEvent.VK_S)) my += 1;
-		if (mx*mx + my*my != 0) {
-			float a = (float) Math.sqrt(mx*mx + my*my);
+		if (mx * mx + my * my != 0) {
+			float a = (float) Math.sqrt(mx * mx + my * my);
 			mx /= a;
 			my /= a;
 		}
@@ -150,8 +160,8 @@ public class GameView extends View implements Controller {
 		if (key.isPressed(KeyEvent.VK_LEFT)) mx -= 1;
 		if (key.isPressed(KeyEvent.VK_UP)) my -= 1;
 		if (key.isPressed(KeyEvent.VK_DOWN)) my += 1;
-		if (mx*mx + my*my != 0) {
-			float a = (float) Math.sqrt(mx*mx + my*my);
+		if (mx * mx + my * my != 0) {
+			float a = (float) Math.sqrt(mx * mx + my * my);
 			mx /= a;
 			my /= a;
 		}
@@ -161,18 +171,21 @@ public class GameView extends View implements Controller {
 
 	@Override
 	public void onKeyType(int i) {
-		if(i == KeyEvent.VK_E) game.getFirstPlayer().shoot();
-		if(i == KeyEvent.VK_MINUS) game.getSecondPlayer().shoot();
+		if (i == KeyEvent.VK_E) game.getFirstPlayer().shoot();
+		if (i == KeyEvent.VK_MINUS) game.getSecondPlayer().shoot();
 	}
 
 	@Override
-	public void onMouseWheel(double v) {}
+	public void onMouseWheel(double v) {
+	}
 
 	@Override
-	public void onMouseDrag(int i, int i1) {}
+	public void onMouseDrag(int i, int i1) {
+	}
 
 	@Override
-	public void onMouseClick(int i, int i1) {}
+	public void onMouseClick(int i, int i1) {
+	}
 
 	@Override
 	public void stop() {
